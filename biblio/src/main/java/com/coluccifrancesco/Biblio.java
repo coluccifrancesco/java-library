ab7afe9f4111013da5ed61a151c55fa4e4402da1
coluccifrancesco <coluccif2002@gmail.com>
2026-02-26 12:26:36 +0100

    first commit

---
+++ biblio/src/main/java/com/coluccifrancesco/Biblio.java
@@ -0,0 +1,18 @@
+package com.coluccifrancesco;
+
+public class Biblio {
+	
+	private Biblio(){
+	}
+
+	/**
+	 * 
+	 * @param args Command line arguments
+	 * @author Francesco Colucci
+	 * @version 0.1
+	 */
+    public static void main(String[] args){
+       
+    }
+
+}
---
+++ biblio/src/test/java/com/coluccifrancesco/BiblioTest.java
@@ -0,0 +1,20 @@
+package com.coluccifrancesco;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for Biblio.
+ */
+class BiblioTest {
+
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testBiblio() {
+        assertEquals(1, 1);
+    }
+
+}
