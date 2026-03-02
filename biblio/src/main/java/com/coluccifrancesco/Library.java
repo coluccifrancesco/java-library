b010b74009a5f039b861cf63263e9574b54181a9
coluccifrancesco <coluccif2002@gmail.com>
2026-03-02 15:03:07 +0100

    Building library: begin

--- biblio/src/main/java/com/coluccifrancesco/Library.java
+++ biblio/src/main/java/com/coluccifrancesco/Library.java
@@ -14,5 +14,13 @@ public class Library {
     
 	public static void main(String[] args){
     
+		// Biblioteca vuota
+
+		// Switch con: 
+		// - aggiunta di un libro, 
+		// - rimozione di un libro, 
+		// - visualizzazione libri, 
+		// - calcolo classici, 
+		// - uscita dal programma
     }
 }
